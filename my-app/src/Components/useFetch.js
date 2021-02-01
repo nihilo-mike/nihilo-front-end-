5b7fd2c4dc005273a4244fed57276d78071fe106
somebulb <69670938+somebulb@users.noreply.github.com>
2021-02-01 07:42:54 -0800

    Reverted changes with TODO added

--- my-app/src/Components/useFetch.js
+++ my-app/src/Components/useFetch.js
@@ -4,13 +4,9 @@ const[loading,setLoading]=useState(true);
 const[data,setData]=useState([]);
 
 const getData=async()=>{
- const response=await fetch(url, {
-     method: 'POST',
-     headers:{
-         Accept: 'application/json',
-         'Content-Type': 'application/json',
-     },
- });
+ const response=await fetch(url);
+
+// TODO: Fetch url with privilege
 
 const data=await response.json();
  setData(data);
